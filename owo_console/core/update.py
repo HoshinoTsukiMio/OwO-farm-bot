7ead2253ef496e06032e5940df5a4b250805156b
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-06-07 21:03:27 +0700

    Update update.py

--- owo_console/core/update.py
+++ owo_console/core/update.py
@@ -1,15 +1,13 @@
-import requests
+import requests, os
+current_dir = os.getcwd()
 
-def update_bot():
-    print("Updating...")
-    update = requests.get(
-        "main file link"
-    )
-
-    # Save the new version to the local file system
-    with open("Main.py", "wb") as f:
-        f.write(update.content)
-    print("Update complete!")
-    #hmm so good
-    #perfect
+# Join the current directory with the filename
+flie_data = os.path.join(current_dir, 'core\\main1.py')
+print("Updating...")
+update = requests.get(
+    "https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-farm-bot/Main/owo_console/core/Main.py"
+)
+with open(flie_data, "wb") as f:
+    f.write(update.content)
+print("Update complete!")
 
