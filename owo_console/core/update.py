529081ffd38885799ff8548bfdd8de5a8fe693b2
Hoshino Tsuki Mio <niisanloli6@gmail.com>
2024-06-07 21:03:41 +0700

    Update update.py

--- owo_console/core/update.py
+++ owo_console/core/update.py
@@ -2,7 +2,7 @@ import requests, os
 current_dir = os.getcwd()
 
 # Join the current directory with the filename
-flie_data = os.path.join(current_dir, 'core\\main1.py')
+flie_data = os.path.join(current_dir, 'core\\main.py')
 print("Updating...")
 update = requests.get(
     "https://raw.githubusercontent.com/HoshinoTsukiMio/OwO-farm-bot/Main/owo_console/core/Main.py"
